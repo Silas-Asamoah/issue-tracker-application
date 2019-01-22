20aee637a0947fc3171a881049c4aae060d05734
Silas Bempong <silasbempong@gmail.com>
2019-01-22 14:54:03 +0000

    addition of endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 import Issue from './models/issue'
+import issue from './models/issue';
+import { runInNewContext } from 'vm';
 
 
 
@@ -25,6 +27,8 @@ connection.once('open', () => {
 
 //Attaching a middleware which is the default router
 //Configure the router with end points to be exposed to the API
+
+//1st EndPoint
 router.route('/issues').get((req, res) => {
     Issue.find((err, issues) => {
         if (err)
@@ -33,6 +37,49 @@ router.route('/issues').get((req, res) => {
             res.json(issues)
     });
 
+});
+
+//2nd Endpoint
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) =>{
+        if (err)
+            console.log(err);
+        else
+            res.json(issue)
+    });
+});
+
+
+//3rd Endpoint adding new issues to the database
+router.route('/issues/add').post((req, res) =>{
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.status(200).json({'issue': 'Added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create a new record')
+        });
+})
+
+//4th EndPoint updating existing issues
+router.route('issues/update/:id').post((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (!issue)
+            return next(new Error('Could not load document'));
+        else
+            issue.title = req.body.title;
+            issue.responsible = req.body.responsible;
+            issue.description = req.body.description;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
+
+            issue.save().then(issue => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+    });
 });
 
 app.use('/', router);
